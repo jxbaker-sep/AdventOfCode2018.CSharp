988fe5aa3acf8247e198af33b86f668f38df68be
Jake Baker <SEP.Baker@fronius.com>
2025-01-09 17:35:18 -0500

    day23

--- Day23.cs
+++ Day23.cs
@@ -28,77 +28,63 @@ public class Day23
   [Theory]
   [InlineData("Day23.sample.2", 36)]
   [InlineData("Day23", 123356173)]
-  // rather shamefacedly copied from https://www.reddit.com/r/adventofcode/comments/a8s17l/comment/ecdqzdg/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-  public void Part2_Magic(string path, int expected)
+  public void Part2(string path, int expected)
   {
     var bots = Convert(AoCLoader.LoadFile(path));
 
-    var q = new PriorityQueue<(long, long)>(it => it.Item1 * 10 + it.Item2);
-    foreach (var bot in bots)
-    {
-      var d = bot.Point.ManhattanDistance(new(0, 0, 0));
-      q.Enqueue((Math.Max(0, d - bot.Radius), 1));
-      q.Enqueue((d + bot.Radius + 1, -1));
-    }
-    long count = 0;
-    long maxCount = 0;
-    long result = 0;
-    while (q.TryDequeue(out var current))
-    {
-      var (dist, e) = current;
-      count += e;
-      if (count > maxCount)
-      {
-        result = dist;
-        maxCount = count;
-      }
-    }
-    result.Should().Be(expected);
-  }
+    var cube = new Cube(new(bots.Min(b => b.Point.X), bots.Min(b => b.Point.Y), bots.Min(b => b.Point.Z)),
+      new(bots.Max(b => b.Point.X), bots.Max(b => b.Point.Y), bots.Max(b => b.Point.Z)));
 
-  [Theory]
-  [InlineData("Day23.sample.2", 36)]
-  [InlineData("Day23", 123356173)]
-  public void Part2_Using_Nearest(string path, int expected)
-  {    
-    var bots = Convert(AoCLoader.LoadFile(path));
+    var result = Hunter(bots, cube);
 
-    var cube = new Cube(new(bots.Min(b => b.Point.X),bots.Min(b => b.Point.Y),bots.Min(b => b.Point.Z)),
-      new(bots.Max(b => b.Point.X),bots.Max(b => b.Point.Y),bots.Max(b => b.Point.Z)));
-
-    var result = Hunter(bots, cube, [(point) => point.X, (point) => point.Y, (point) => point.Z],
-      [(point, value) => point with {X=value},(point, value) => point with {Y=value},(point, value) => point with {Z=value}]);
-
-    // Console.WriteLine(result);
     result.Item1.Little.ManhattanDistance(Point3.Zero).Should().Be(expected);
   }
 
-  private static (Cube, long) Hunter(List<Bot> bots, Cube inputCube, List<Func<Point3, long>> inputGetters, List<Func<Point3, long, Point3>> inputSetters)
+  private static (Cube, long) Hunter(List<Bot> bots, Cube inputCube)
   {
-    PriorityQueue<(Cube cube, long order, List<Func<Point3, long>> getters, List<Func<Point3, long, Point3>> setters)> q = new(it => -it.order);
+    // Algorithm:
+    //  start with a q of the input cube
+    //  take the cube from the q that has the largest number of overlapping bot ranges.
+    //  if that cube is of size 1, then we've found the target (theoritically there could be closer targets of the same order, but not in this data, haha)
+    //  otherwise, add the 8 sub-cubes of this cube to the q
+    PriorityQueue<(Cube cube, long order)> q = new(it => -it.order);
 
     long NumberOfBots(Cube c) => bots.Count(b => b.Overlaps(c));
-    q.Enqueue((inputCube, NumberOfBots(inputCube), inputGetters, inputSetters));
+    q.Enqueue((inputCube, NumberOfBots(inputCube)));
 
     while (q.TryDequeue(out var current))
     {
-      // Console.WriteLine(current.order);
-      var getter = current.getters[0];
-      var setter = current.setters[0];
-      var a = getter(current.cube.Little);
-      var b = getter(current.cube.Big);
-      Console.WriteLine($"{current.cube}, {current.order}, {a}, {b}");
-      if (a == b) {
-        if (current.getters.Count == 1) return (current.cube, current.order);
-        q.Enqueue((current.cube, current.order, current.getters[1..], current.setters[1..]));
-        continue;
+      Console.WriteLine($"{current.order}: {current.cube.Order} {current.cube}");
+      var a = current.cube.Little.X;
+      var b = current.cube.Big.X;
+      var c = current.cube.Little.Y;
+      var d = current.cube.Big.Y;
+      var e = current.cube.Little.Z;
+      var f = current.cube.Big.Z;
+      if (a == b && c == d && e == f)
+      {
+        return (current.cube, current.order);
       }
-      a.Should().BeLessThan(b);
-      var split = (a + b) / 2;
-      var left = current.cube with {Big = setter(current.cube.Big, split)};
-      var right = current.cube with {Little = setter(current.cube.Little, split + 1)};
-      q.Enqueue((left, NumberOfBots(left), current.getters, current.setters));
-      q.Enqueue((right, NumberOfBots(right), current.getters, current.setters));
+      List<Cube> cubes = [current.cube];
+      if (a != b)
+      {
+        var split = (a + b) / 2;
+        cubes = cubes.SelectMany(cube => new[] { cube with { Big = cube.Big with { X = split } }, cube with { Little = cube.Little with { X = split + 1 } } }).ToList();
+      }
+      if (c != d)
+      {
+        var split = (c + d) / 2;
+        cubes = cubes.SelectMany(cube => new[] { cube with { Big = cube.Big with { Y = split } }, cube with { Little = cube.Little with { Y = split + 1 } } }).ToList();
+
+      }
+      if (e != f)
+      {
+        var split = (e + f) / 2;
+        cubes = cubes.SelectMany(cube => new[] { cube with { Big = cube.Big with { Z = split } }, cube with { Little = cube.Little with { Z = split + 1 } } }).ToList();
+
+      }
+      cubes.Count.Should().BeGreaterThan(1);
+      foreach (var cube in cubes) q.Enqueue((cube, NumberOfBots(cube)));
     }
     throw new ApplicationException();
   }
@@ -113,7 +99,10 @@ public class Day23
     }
   }
 
-  public record Cube(Point3 Little, Point3 Big);
+  public record Cube(Point3 Little, Point3 Big)
+  {
+    public BigInteger Order => new BigInteger(1 + Big.X - Little.X) * new BigInteger(1 + Big.Y - Little.Y) * new BigInteger(1 + Big.Z - Little.Z);
+  }
 
   public record Bot(Point3 Point, long Radius)
   {
